ba92f19b0beaf0c423ed8d8f7e1cb3e8fdb93456
Judd <foldl@boxvest.com>
2025-01-08 10:43:47 +0800

    check for coll for cyclic

--- src/numericals.py
+++ src/numericals.py
@@ -705,6 +705,8 @@ def check_perp(points: list[Point]) -> bool:
 def check_cyclic(points: list[Point]) -> bool:
     points = list(set(points))
     a, b, c, *ps = points
+    if check_coll([a, b, c]):
+        return False
     circle = Circle(p1=a, p2=b, p3=c)
     for d in ps:
         if not close_enough(d.distance(circle.center), circle.radius):
